c0b4825da41b5fbf621fa094dcc536612000535f
theianreis <ianreisdocs@gmail.com>
2026-02-05 22:18:22 -0300

    resolução

--- src/Contagem.java
+++ src/Contagem.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Contagem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite N: ");
+        int N = scanner.nextInt();
+
+        for (int i = N; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+    }
 }
--- src/ParouImpar.java
+++ src/ParouImpar.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ParouImpar {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor para avaliação (Par/Ímpar): ");
+        int N = scanner.nextInt();
+
+        if (N % 2 != 0) {
+            System.out.println("O número é ímpar");
+        } else {
+            System.out.println("O número é par");
+        }
+
+        scanner.close();
+    }
 }
+
+
+
