0b2c4b37c85c9617fade4631ea83be9fc088dad7
theianreis <ianreisdocs@gmail.com>
2026-02-05 21:44:14 -0300

    Resolução de problema: Soma dos multiplos

---
+++ src/SomaMultiplos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SomaMultiplos {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro positivo: ");
+        int N = scanner.nextInt();
+
+        int soma = 0;
+
+        for (int i = 1; i < N; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                soma += i;
+            }
+        }
+
+        System.out.println("A soma dos múltiplos de 3 ou 5 menores que " + N + " é: " + soma);
+
+        scanner.close();
+    }
+}
